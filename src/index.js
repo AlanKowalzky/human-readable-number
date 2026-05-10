a228291a127362804fdd86840b1b83f4f2154c86
AlanKowalzky <alan.kowalzky@gmail.com>
2026-05-10 20:33:21 +0200

    feat: app test lint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const units = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  if (number < 20) return units[number];
+
+  let result = '';
+  let n = number;
+
+  // Obsługa setek
+  if (n >= 100) {
+    result += `${units[Math.floor(n / 100)]} hundred`;
+    n %= 100;
+    if (n === 0) return result;
+    result += ' ';
+  }
+
+  // Obsługa dziesiątek i jednostek po setkach
+  if (n < 20) {
+    result += `${units[n]}`;
+  } else {
+    result += `${tens[Math.floor(n / 10)]}`;
+    if (n % 10 > 0) {
+      result += ` ${units[n % 10]}`;
+    }
+  }
+
+  return result.trim();
 };
